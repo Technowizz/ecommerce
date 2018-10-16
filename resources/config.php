7e254dc841f72fabe840ff84752f6b9c1c2b4aaf
kuttyselva <kuttyselva57@gmail.com>
2018-10-17 01:04:13 +0530

    update

--- resources/config.php
+++ resources/config.php
@@ -13,6 +13,7 @@ defined("DB_USER") ? null : define("DB_USER","root");
 defined("DB_PASS") ? null : define("DB_PASS","");
 defined("DB_NAME") ? null : define("DB_NAME","ecommerce_db");
 $connection = musqli_connection(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
 require_once("functions.php")
 
 
